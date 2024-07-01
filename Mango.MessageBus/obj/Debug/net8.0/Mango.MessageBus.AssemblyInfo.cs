d8fa40d819e42ab4420d29cb0497fd12ae00869b
Lê Quốc Hưng <98202931+hungle2002@users.noreply.github.com>
2024-07-01 21:48:01 +0700

    [Harry] Implement email service to write email to db

--- Mango.MessageBus/obj/Debug/net8.0/Mango.MessageBus.AssemblyInfo.cs
+++ Mango.MessageBus/obj/Debug/net8.0/Mango.MessageBus.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.MessageBus")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd66e41fe253db17c8975ef599f614c6e7a78e91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+653f2f3a877412493e3489fe106c2ca6b2d9e2ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.MessageBus")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.MessageBus")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
+++ Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd66e41fe253db17c8975ef599f614c6e7a78e91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+653f2f3a877412493e3489fe106c2ca6b2d9e2ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
